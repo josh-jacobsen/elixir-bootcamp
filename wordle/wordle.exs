e19dc7f1f26be722372ab77b8f179858f7d48359
Josh <josh.jacobsen@cogo.co>
2022-09-27 15:40:47 +1300

    Add modules for different functions

--- wordle/wordle.exs
+++ wordle/wordle.exs
@@ -1,48 +1,45 @@
-defmodule Wordle do
+defmodule Wordle.Service do
   @guesses 6
+  @all_valid_words_file_path "/Users/josh/Downloads/all_words.txt"
+  @target_word_file_path "/Users/josh/Downloads/words.txt"
+
   def start() do
-    {:ok, words} = File.read("/Users/josh/Downloads/words.txt")
-    {:ok, all_words} = File.read("/Users/josh/Downloads/all_words.txt")
+    #  Abstraction level
+    # Module extraction
+    target_word = load_target_word(@target_word_file_path)
+    all_valid_words = load_all_valid_words(@all_valid_words_file_path)
 
-    words
-    |> String.split()
-    |> Enum.random()
-    # |> IO.inspect()
-    |> game_loop(all_words, @guesses)
+    game_loop(target_word, all_valid_words, @guesses)
   end
 
-  def game_loop(word, _all_words, 0) do
-    IO.puts("You lost")
-    IO.puts("The word was #{word}")
+  def game_loop(target_word, _all_words, 0) do
+    Wordle.UI.inform_user_of_defeat(target_word)
   end
 
-  def game_loop(word, all_words, guesses_remaining) do
-    IO.puts("You have #{guesses_remaining} guesses remaining")
-    guess = prompt_user_for_guess()
+  def game_loop(target_word, all_words, guesses_remaining) do
+    guess = Wordle.UI.prompt_user_for_guess(guesses_remaining)
 
-    with :ok <- validate_guess_length(guess),
-         :ok <- validate_guess_is_in_all_words_list(guess, all_words),
-         :ok <-
-           validate_has_guess_won(guess, word) do
-      guess
-      |> apply_colors(word)
-      |> Enum.reverse()
-      |> IO.puts()
+    case Wordle.Validation.validate_guess(guess, all_words, target_word) do
+      {:ok, guess} ->
+        apply_colors(guess, target_word)
+        |> Enum.reverse()
+        |> IO.puts()
 
-      game_loop(word, all_words, guesses_remaining - 1)
-    else
-      {:ok, :won} ->
-        IO.puts("Congrats!! You won -- the word was #{IO.ANSI.format([:green, word])}")
+        game_loop(target_word, all_words, guesses_remaining - 1)
+
+      :won ->
+        Wordle.UI.inform_user_of_victory(target_word)
 
       {:error, reason} ->
-        IO.puts("Things blew up becasue #{reason}")
-        game_loop(word, all_words, guesses_remaining)
+        Wordle.UI.inform_user_of_error(reason)
+        game_loop(target_word, all_words, guesses_remaining)
     end
   end
 
   defp apply_colors(guess, word) do
     word_graphemes = String.graphemes(word)
 
+    # No more than 3 layers deep
     Enum.reduce(String.graphemes(guess), [], fn value, acc ->
       if value in word_graphemes do
         case Enum.fetch(word_graphemes, length(acc)) do
@@ -62,33 +59,48 @@ defmodule Wordle do
               if yellow_letters_in_acc + green_letters_in_acc < total_letters_in_word do
                 [IO.ANSI.format([:yellow, value]) | acc]
               else
-                [IO.ANSI.format([:black, value]) | acc]
+                [IO.ANSI.format([:red, value]) | acc]
               end
             end
         end
       else
-        [IO.ANSI.format([:black, value]) | acc]
+        [IO.ANSI.format([:red, value]) | acc]
       end
     end)
   end
 
-  defp prompt_user_for_guess() do
-    IO.gets("Enter your guess: ")
-    |> String.trim()
-    |> String.downcase()
+  defp load_target_word(path) do
+    {:ok, words} = File.read(path)
+
+    words
+    |> String.split()
+    |> Enum.random()
+    |> IO.inspect()
   end
 
-  defp validate_has_guess_won(guess, word) do
-    if guess == word do
-      {:ok, :won}
+  defp load_all_valid_words(path) do
+    {:ok, words} = File.read(path)
+    String.split(words)
+  end
+end
+
+defmodule Wordle.Validation do
+  def validate_guess(guess, all_words, target_word) do
+    with :ok <- validate_guess_length(guess),
+         :ok <- validate_guess_is_in_all_words_list(guess, all_words),
+         :in_progress <- has_guess_won(guess, target_word) do
+      {:ok, guess}
     else
-      :ok
+      :won ->
+        :won
+
+      {:error, reason} ->
+        {:error, reason}
     end
   end
 
-  # Tried a few different approaches to writing this validation function. Since String.length can't be used in a guard,
-  # and pattern matching on the input list would get verbose, this was the most elegant implementation I could come up with
   defp validate_guess_length(guess) do
+    # make this better
     cond do
       String.length(guess) == 5 -> :ok
       String.length(guess) < 5 -> {:error, :guess_too_short}
@@ -97,27 +109,51 @@ defmodule Wordle do
     end
   end
 
-  # defp validate_guess_length(guess) when String.length(guess) < 5 do
-  #   {:error, :guess_too_short}
-  # end
-
-  # defp validate_guess_length([x1, x2, x3, x4, x5] = guess) do
-  #   :ok
-  # end
-
-  # defp validate_guess_length(guess) do
-  #   {:error, :guess_length_not_valid}
-  # end
-
   defp validate_guess_is_in_all_words_list(guess, all_words) do
-    word_list = String.split(all_words)
-
-    if Enum.member?(word_list, guess) do
+    if Enum.member?(all_words, guess) do
       :ok
     else
       {:error, :guess_not_valid_word}
     end
   end
+
+  defp has_guess_won(target_word, target_word), do: :won
+  defp has_guess_won(_, _), do: :in_progress
 end
 
-Wordle.start()
+defmodule Wordle.UI do
+  def inform_user_of_defeat(target_word) do
+    IO.puts("You lost")
+    IO.puts("The word was #{target_word}")
+  end
+
+  def inform_user_of_victory(target_word) do
+    IO.puts("Congrats!! You won -- the word was #{IO.ANSI.format([:green, target_word])}")
+  end
+
+  def inform_user_of_error(reason) do
+    case reason do
+      :guess_too_short ->
+        IO.puts("The guess was too short")
+
+      :guess_too_long ->
+        IO.puts("The guess was too long")
+
+      :guess_not_valid_word ->
+        IO.puts("That isn't a valid word")
+
+      _ ->
+        IO.puts("There was an issue with your guess. Please check the input and try again")
+    end
+  end
+
+  def prompt_user_for_guess(guesses_remaining) do
+    IO.puts("You have #{guesses_remaining} guesses remaining")
+
+    IO.gets("Enter your guess: ")
+    |> String.trim()
+    |> String.downcase()
+  end
+end
+
+Wordle.Service.start()
